91d7968bc3949aff06fe5d4c8f2db40cbe0e2940
armin2006n <armin_juju@hotmail.com>
2013-11-06 20:34:10 +0100

    init project

---
+++ StackUnderflow.Common/BL/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using StackUnderflow.Common.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUnderflow.Common.BL
+{
+    public interface IUserService
+    {
+        IQueryable<User> GetAllUsers(int page);
+    }
+}
---
+++ StackUnderflow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StackUnderflow.MvcApplication" Language="C#" %>
