515ad5e7bb91bceaf73f446dcda181b2a88811f6
armin2006n <armin_juju@hotmail.com>
2013-11-06 20:21:43 +0100

    init commit

---
+++ StackUnderflow.Common/BL/UserService.cs
@@ -0,0 +1,38 @@
+﻿using StackUnderflow.Common.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUnderflow.Common.BL
+{
+    class UserService : IUserService
+    {
+        private DAL.stackunderflowEntities _dal;
+        public UserService(StackUnderflow.Common.DAL.stackunderflowEntities dal)
+        {
+            _dal = dal;
+        }
+
+        public IQueryable<User> GetAllUsers(int page)
+        {
+            return _dal.User
+                .Where(i => i.registered == true)
+                .OrderBy(i => i.user_name)
+                .Skip(page * 50)
+                .Take(50);
+        }
+
+        public User GetUser(int id)
+        {
+            var user = _dal.User.FirstOrDefault(i => i.user_id == id);
+            return user;
+        }
+
+        public void Save()
+        {
+            _dal.SaveChanges();
+        }
+    }
+}
