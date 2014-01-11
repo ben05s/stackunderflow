143eec71a3a12f4146b1a77bc014cccc1c95eda5
benny99 <b.s80@gmx.at>
2014-01-11 20:07:26 +0100

    interface

--- StackUnderflow.Common/BL/IUserService.cs
+++ StackUnderflow.Common/BL/IUserService.cs
@@ -9,6 +9,22 @@ namespace StackUnderflow.Common.BL
 {
     public interface IUserService
     {
-        IQueryable<User> GetAllUsers(int page);
+        /*USER methods*/
+        Boolean CreateUser(string username, string password, string email);
+        Boolean RegisterUser(string username);
+        Boolean LoginUser(string username, string password);
+        Boolean UpdatePassword(string username, string newpassword, string oldpassword);
+
+        IQueryable<User> GetAllUsers();
+        IQueryable<User> SearchForUsers(string query);
+        User GetUser(int user_id);
+
+        /*QUESTION & ANSWER methods*/
+        IQueryable<Question> GetAllQuestions();
+        Boolean CreateQuestion(string username, string title, string content);
+        IQueryable<Question> SearchForQuestions(string query);
+
+
+        void Save();
     }
 }
