209289ec5d5a2602dd76529d71fdd765a76faf82
benny99 <b.s80@gmx.at>
2014-01-11 21:02:16 +0100

    interface public methods

--- StackUnderflow.Common/BL/UserService.cs
+++ StackUnderflow.Common/BL/UserService.cs
@@ -12,22 +12,23 @@ namespace StackUnderflow.Common.BL
         private DAL.stackunderflowEntities _dal = new DAL.stackunderflowEntities();
         public UserService() { }
 
-        Boolean CreateUser(string username, string password, string email)
+        public Boolean CreateUser(string username, string password, string email)
+        {
+            var usr = new User();
+            return false;
+        }
+
+        public Boolean RegisterUser(string username)
         {
             return false;
         }
 
-        Boolean RegisterUser(string username)
+        public Boolean LoginUser(string username, string password)
         {
             return false;
         }
 
-        Boolean LoginUser(string username, string password)
-        {
-            return false;
-        }
-
-        Boolean UpdatePassword(string username, string newpassword, string oldpassword)
+        public Boolean UpdatePassword(string username, string newpassword, string oldpassword)
         {
             return false;
         }
@@ -47,37 +48,37 @@ namespace StackUnderflow.Common.BL
                 .Take(50);
         }
 
-        IQueryable<User> SearchForUsers(string query, int page)
+        public IQueryable<User> SearchForUsers(string query, int page)
         {
             return null;
         }
 
-        IQueryable<Question> GetAllQuestions(int page)
+        public IQueryable<Question> GetAllQuestions(int page)
         {
             return null;
         }
 
-        IQueryable<Question> SearchForQuestions(string query, int page)
+        public IQueryable<Question> SearchForQuestions(string query, int page)
         {
             return null;
         }
 
-        Boolean CreateQuestion(string username, string title, string content)
+        public Boolean CreateQuestion(string username, string title, string content)
         {
             return false;
         }
 
-        Boolean CreateAnswer(int user_id, int question_id, string content)
+        public Boolean CreateAnswer(int user_id, int question_id, string content)
         {
             return false;
         }
 
-        int RateUpAnswer(int answer_id)
+        public int RateUpAnswer(int answer_id)
         {
             return 0;
         }
 
-        int RateDownAnswer(int answer_id)
+        public int RateDownAnswer(int answer_id)
         {
             return 0;
         }
